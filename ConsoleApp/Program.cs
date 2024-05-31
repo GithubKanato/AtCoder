05a8814d3ab4afe1485c4d27d30dd9ef2250606c
GithubKanatom <detectiveboys710@gmail.com>
2024-05-31 23:31:07 +0900

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // スペース区切りの整数の入力
+        string input = Console.ReadLine();
+        char[] splitStr = input.ToCharArray();
+        int count = 0;
+        for(int i = 0 ;i <= splitStr.Length-1;i++){
+            if(splitStr[i] == '1'){
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
