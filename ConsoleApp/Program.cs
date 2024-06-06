5fe97d6ba0c8ba9df14d17652c168a2ac08b9411
GithubKanatom <detectiveboys710@gmail.com>
2024-06-06 22:43:01 +0900

    ABC087B

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,14 +1,45 @@
 ﻿using System;
+using System.Drawing;
+
 class Program
 {
     static void Main(string[] args)
     {
-        // スペース区切りの整数の入力
-        string input = Console.ReadLine();
-        char[] splitStr = input.ToCharArray();
+        Program program = new Program();
+        program.ABC087B();
+    }
+
+    private void ABC087B()
+    {
+        string input_A = Console.ReadLine();
+        string input_B = Console.ReadLine();
+        string input_C = Console.ReadLine();
+        string input_X = Console.ReadLine();
+
+        List<int> list = new List<int>();
+        // Aは500円
+        // Bは100円
+        // Cは50円
+        for (int a = 0; a <= int.Parse(input_A); a++)
+        {
+            for (int b = 0; b <= int.Parse(input_B); b++)
+            {
+                list.Add(a * 500 + b * 100);
+            }
+        }
+        List<int> sumList = new List<int>();
+        for (int c = 0; c <= int.Parse(input_C); c++)
+        {
+            foreach(int li in list)
+            {
+                sumList.Add(li + c * 50);
+            }
+        }
         int count = 0;
-        for(int i = 0 ;i <= splitStr.Length-1;i++){
-            if(splitStr[i] == '1'){
+        foreach(int data in sumList)
+        {
+            if (data == int.Parse(input_X))
+            {
                 count++;
             }
         }
---
+++ ConsoleApp/Who_Ate_the_Cake.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Who_Ate_the_Cake
+    {
+        static void Who_Ate_the_Cake_Main()
+        {
+            // スペース区切りの整数の入力
+            string input_raw = Console.ReadLine();
+            string[] input = input_raw.Split(" ");
+            // 1~3
+            string result = "123";
+            foreach (string s in input)
+            {
+                result = result.Replace(s.Trim(), "");
+            }
+            if (result.Length > 1)
+            {
+                result = "-1";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
