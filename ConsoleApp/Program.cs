328e458a99abec9f18d274acbba5c54b793f1da1
GithubKanatom <detectiveboys710@gmail.com>
2024-06-06 22:59:44 +0900

    ABC083B

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,8 +6,35 @@ class Program
     static void Main(string[] args)
     {
         Program program = new Program();
-        program.ABC087B();
+        program.ABC083B();
     }
+
+    private void ABC083B()
+    {
+        string input_raw = Console.ReadLine();
+        string[] input = input_raw.Split(" ");
+        int input_N = int.Parse(input[0]);
+        int input_A = int.Parse(input[1]);
+        int input_B = int.Parse(input[2]);
+
+        int count = 0;
+        for (int i = 1; i <= input_N; i++)
+        {
+            char[] splitStr = i.ToString().ToCharArray();
+            int sum = 0;
+            foreach (char c in splitStr)
+            {
+                sum += int.Parse(c.ToString());
+            }
+            if (input_A <= sum && input_B >= sum)
+            {
+                count += i;
+            }
+        }
+        Console.WriteLine(count);
+
+    }
+
 
     private void ABC087B()
     {
@@ -30,13 +57,13 @@ class Program
         List<int> sumList = new List<int>();
         for (int c = 0; c <= int.Parse(input_C); c++)
         {
-            foreach(int li in list)
+            foreach (int li in list)
             {
                 sumList.Add(li + c * 50);
             }
         }
         int count = 0;
-        foreach(int data in sumList)
+        foreach (int data in sumList)
         {
             if (data == int.Parse(input_X))
             {
